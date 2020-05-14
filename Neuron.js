11f533061227f378cc93d38744ae07a8e6d2fa78
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-14 13:30:16 -0300

    Correção de bugs no neuronio

--- Neuron.js
+++ Neuron.js
@@ -1,59 +1,50 @@
-let Neuron = {
+function Neuron(){
   //Data
-  weight : [],
-  bias : 0,
-  trigger : 0,
-  score : 0,
+  this.weight = [];
+  this.bias = 0;
+  this.trigger = 0;
+  this.score = 0;
 
   //Cria um neuronio vazio
-  generate : function(size = 1){
+  this.generate = (size = 1) => {
     for(let i=0; i<size; i++){
       this.weight.push(randomRange(-1, 1));
     }
-    this.bias = Math.random();
+    this.bias = randomRange(0, 100);
     this.trigger = Math.random();
-  },
+  };
 
   //Função de ativação
-  g : function(sign_array){
+  this.g = (sign_array) => {
       let u = 0;
       //Atribui o peso à cada sinal
       for(let i=0; i<sign_array.length; i++){
         if(i == this.weight.length) break;
-        u += tgh(sign_array[i]) * this.weight[i];
+        u += sign_array[i] * this.weight[i];
       }
       //Somatória dos sinais - bias
       u -= this.bias;
 
-      if(u > this.trigger) return 1; //Ativado
-      else if(u < -this.trigger) return -1;
+      if(u > this.trigger) return 1; //Ativado positivo
+      else if(u < -this.trigger) return -1; //Ativado negativo
       else return 0;// não ativado
-  },
+  };
 
   //Provoca Mutação
-  mutation : function(){
+  this.mutation = () => {
     //Pesos
-    for(let i=0; i<this.weight.length; i++){
-      if(Math.random() <= .2) this.weight += randomRange(-1, 1);
+    for(let k=0; k<this.weight.length; k++){
+      if(Math.random() <= .2) this.weight[k] += randomRange(-1, 1);
     }
     //Bias
     if(Math.random() <= .2) this.bias += randomRange(-1, 1);
     //Trigger
     if(Math.random() <= .2) this.trigger += randomRange(-1, 1);
-  },
-
-  //Retorna informações do neurônio
-  getNeuron : function(){
-    return {
-      weight : this.weight,
-      bias : this.bias,
-      trigger : this.trigger
-    }
-  }
+  };
 }
 
 function tgh(number){
-  return (1 - 2/Math.pow(Math.E, number) + 1);
+  return 1 - 2/(Math.pow(Math.E, number) + 1);
 }
 
 function randomRange(min, max){
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const neuron = require("./Neuron.js");
+let neuron = require("./Neuron.js");
 const dinosaur = require("./Dinosaur");
 
 //Configurations
 let configs = {
-  dinos : 10,
+  dinos : 5,
   fps : 16
 };
 
@@ -14,17 +14,15 @@ let room_speed = 0;
 let dinoAI = [];
 
 for(let i=0; i<configs.dinos; i++){
-  dinoAI[i] = Object.create(neuron);
+  dinoAI[i] = new neuron();
   dinoAI[i].generate(2);
 }
-
-console.log(dinoAI);
 
 //Setup
 let setup = {};
 let runSetup = () => {
   //Verifica se está na tela do jogo
-  if(dinosaur.checkGameScreen()){
+  if(dinosaur.checkGameScreen() && dinosaur.checkGameOver()){
     runLoop();
     timer();
     return;
@@ -36,29 +34,80 @@ let runSetup = () => {
 //Loop
 let loop = {};
 let runLoop = () => {
-  if(dinosaur.checkGameOver() && room_speed > 3){
+
+  if(current_generation == configs.dinos){
+    clearTimeout(loop);
+    clearTimeout(timer);
+    createNewGeneration();
+    return;
+  } else if (dinosaur.checkGameOver() && room_speed > 3){
+
     dinosaur.move(0);//começa o jogo
     dinoAI[current_generation].score = room_speed;
-    console.log(current_generation);
-    if(current_generation === configs.dinos){
-      clearTimeout(loop);
-      return;
-    }
     room_speed = 0;
     current_generation++;
-  }else if(!dinosaur.checkGameOver()){
+
+  } else if (!dinosaur.checkGameOver()){
+
     //Verifica se existe obstáculo
     let distance = dinosaur.getObstacleDistance();
     if(distance != -1){
-      //Verifica o sinal do neurônio e executa uma ação
-      let signals = [distance, room_speed];//Distancia e velocidade
+
+      /*
+      * Verifica o sinal do neurônio e executa uma ação
+      * Como a distância representa números muito grandes e a velocidade número muito pequeno
+      * decidi modular os valores da distância dividindo por 10
+      */
+      
+      let signals = [ 
+        distance*.65, //Distância
+        room_speed //Velocidade
+      ];
+
       signals = dinoAI[current_generation].g(signals);
       if(signals == 1) dinosaur.move(0);//pula
       else if (signals == -1) dinosaur.move(1);//abaixa
+
+    }
+
+  }
+  loop = setTimeout(runLoop, 1000/configs.fps);
+}
+
+//New Generation
+let createNewGeneration = () => {
+  let best_neuron = new neuron();
+  let best_id = -1;
+
+  //Pega o melhor da última geração
+  for(let i=0; i<dinoAI.length; i++){
+    if(best_neuron.score < dinoAI[i].score && dinoAI[i].score > 6){
+      best_id = i;
+      best_neuron.weight = dinoAI[i].weight;
+      best_neuron.bias = dinoAI[i].bias;
+      best_neuron.trigger = dinoAI[i].trigger;
+      best_neuron.score = dinoAI[i].score;
     }
   }
-  clearTimeout(loop);
-  loop = setTimeout(runLoop, 1000/configs.fps);
+
+  //Verifica se nenhum dinossauro teve sucesso, caso positivo, gera novos pesos aleatórios
+  if(best_id == -1){
+    console.log("Nenhum aprovado... Gerando novos");
+    for(let i=0; i<configs.dinos; i++){
+      dinoAI[i] = new neuron();
+      dinoAI[i].generate(2);
+    }    
+  } else //Senão substitui a geração por clones do melhor da última geração e gera alguma mutação nos descendentes
+  for(let i=0; i<dinoAI.length; i++){
+    dinoAI[i].weight = best_neuron.weight;
+    dinoAI[i].bias = best_neuron.bias;
+    dinoAI[i].trigger = best_neuron.trigger;
+    dinoAI[i].score = best_neuron.score;
+    //provoca mutação em todos, menos no primeiro
+    if(i>0) dinoAI[i].mutation();
+  }
+  current_generation = 0;
+  runSetup();
 }
 
 //Room Speed
